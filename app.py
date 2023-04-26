02b77785421a1785f1857cdd3c642dacadce27a1
Monish <118999463+monish78@users.noreply.github.com>
2023-04-26 18:57:07 +0530

    ChatBot
    Developed a ChatBot using OpenAI API and Flask for a basic project, aimed to brush up skills on APIs, Flask, and web design.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import openai
+openai.api_key = "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"
+
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    output = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=userText,
+        temperature=0.7,
+        max_tokens=2000,
+        n=1,
+        stop=None,
+    )    
+    return output.choices[0].text
+if __name__ == "__main__":
+    app.run()
+
+
+
